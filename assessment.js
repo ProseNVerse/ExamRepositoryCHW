4127e654d4aee11f8887b7912cf7ae24edcf5e73
ProseNVerse <walker.atlas@yahoo.com>
2018-04-20 15:42:15 +0100

    exam complete and materials

---
+++ assessment.js
@@ -0,0 +1,84 @@
+
+function isPrime(number){
+
+
+
+    let x = 11;
+    for (i = 1; i <=100; i++);
+
+if (x == 1){
+    console.log(x + "  is not a prime number");
+
+}
+else if (x % i == 0 && i != x,1){
+    console.log(x + " is not a prime number");
+}
+
+else if (x == i) {
+        console.log(x + " is a prime number");
+    }
+}
+
+
+ 
+
+    //Prime numbers used for testing: 5,53,177
+    //Non-Prime Numbers used for testing: 6, 12, 200, 2500
+    //if x modular i is equal to 0 and i is not equal to x or 1
+
+    //finding a way to check if x / any other number is an integer
+
+  
+function amountToCoins(amount, coins){
+
+    let a = document.getElementById("CoinInput").value;
+    let b = 25;
+    let c = 10;
+    let d = 5;
+    let e = 2;
+    let f = 1;
+
+    //Transferring 46 cents into amounts of coins where coins available are
+    //25, 10, 5, 2, and 1
+
+    console.log(a % b);
+
+}
+
+function uniqueLetters(string){
+
+    let x = "thequickbrownfoxjumpsoverthelazydog";
+    let y = "thequickbrownfxjmpsvlazydg";
+
+
+    console.log(string(x));
+
+    //string identification and deletion
+
+}
+
+function factorial(number){
+
+    let x = 1;
+    let i = 1;
+    for (i = 1; i <20; i++);
+
+    console.log(i*i);
+
+
+    }
+
+// x multiplied by all previous numbers
+
+
+function fibonacci(number){
+
+//i + (first previous iteration + second previous iteration)
+
+for (i = 1; i>0; i++);
+
+    console.log(i + ((i-1)+(i-2)));
+
+
+    }
+
